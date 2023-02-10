e91433ba68d461d50cd9d9af546a91c4f4625a6d
JacobitKashala <Jacobitkashala@gmail.com>
2023-02-10 15:23:53 +0100

    refactor:code

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+require'vendor/autoload.php';
+// require'src/Router.php';
+echo($_GET['url']);
+die($_GET['url']);
+
+
+$router = new App\Router($_GET['url']);
+
+$router->get("/",function(){echo "Tous les articles";});
+$router->get("/posts/:id",function(){echo "Tous les articles";});
+
+$router->run();
+
