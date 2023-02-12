adfe3e096ff05a16fb4792066bb560585129de09
JacobitKashala <Jacobitkashala@gmail.com>
2023-02-12 10:02:49 +0100

    reorganisation code

--- public/index.php
+++ public/index.php
@@ -1,11 +1,33 @@
 <?php
 require '../vendor/autoload.php';
 
+
+
+$uri = $_SERVER['REQUEST_URI'];
+
 $router = new AltoRouter();
 
-define('VIEW_PATH', dirname(__DIR__) . '/views');
+require '../config/route.php';
 
-$router ->map('GET','/',function(){
-	require VIEW_PATH . '/';
-});
+$match = $router->match();
+	// echo '<pre>';
+	// var_dump($match);
+	// echo '</pre>';
 
+// if ($match !== null) {
+if ($match) {
+	// require '../elements/tete.php';
+	//call_user_func_array($match['target'],$math['params']);
+	if (is_callable($match['target'])) {
+		
+		$match['target']($match['params']['slug'], $match['params']['id']);
+	} else {
+		$params =$match['params'];
+		ob_start();
+		require "../templates/{$match['target']}.php";
+		$pageContent = ob_get_clean();
+	}
+	require '../elements/layout.php';
+} else {
+	echo "404";
+}
