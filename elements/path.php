d13f68d8a4e12a97889346b6adf080dc5b57c4c8
JacobitKashala <Jacobitkashala@gmail.com>
2023-02-12 05:22:06 +0100

    modification home

--- elements/path.php
+++ elements/path.php
@@ -3,7 +3,7 @@ define('DOMAIN_URL', 'https://nom_domain.com/');
 define('path_media', ''); /* chnage to your domain here - don't forget to add forward slash at the end of the URL like this "/" */
 define('path_icon', '/media/icons');
 define('LOGO', '/media/logo');
-define('VIDEO', '/media/video');
-define('BACKGROUND', '/media/background');
+define('path_evideo', '/media/evideo');
+define('path_ephoto', '/media/ephoto');
 define('ILLUSTRATION', '/media/illustration');
 define('LOGOPARTNAIRE', '/media/logo_partnaire');
---
+++ index .php
@@ -0,0 +1,39 @@
+<?php
+require 'vendor/autoload.php';
+$uri =$_SERVER['REQUEST_URI'];
+// map homepage
+$router->map('GET', '/', function() {
+    require __DIR__ . '/views/home.php';
+});
+
+// dynamic named route
+$router->map('GET|POST', '/users/[i:id]/', function($id) {
+  // $user = .....
+  require __DIR__ . '/views/user/details.php';
+}, 'user-details');
+//die($_GET['url']);
+// echo "Bonjour";
+// define('VIEW_PATH', dirname(__DIR__) . '/views');
+
+// $router = new App\Router($_GET['url']);
+
+// $router->get('/', function () {
+// 	require '/views/index.php';
+// });
+
+// $router->get('/exp', function () {
+// 	require 'views/expertise.php';
+// });
+// $router->get('/posts/:slug-:id', function ($slug, $id) {
+// 	echo "Page poste $slug: $id";
+// });
+// $router->post('/posts', function () {
+// 	echo "Voila l'article";
+// });
+// $router->post('/posts/:id', function ($id) {
+// 	echo "Voila l'article $id";
+// });
+
+// $router->run();
+
+// $match =$router->match();
