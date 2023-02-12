25a2cff28e921cb568f2ff7c6bb9829f042e7915
JacobitKashala <Jacobitkashala@gmail.com>
2023-02-12 16:08:41 +0100

    refactor:media

--- public/path.php
+++ public/path.php
@@ -1,8 +1,8 @@
 <?php
 define('DOMAIN_URL', 'https://nom_domain.com/'); 
-define('evideo','./media/video');
-define('ephoto','./media/photo');
-define('logo','./media/logo');
-define('ecrousel','./media/carousel');
-define('icones','./media/icons');
-define('background','./media/background');
+define('evideo','./media/video/');
+define('ephoto','./media/photo/');
+define('logo','./media/logo/');
+define('ecrousel','./media/carousel/');
+define('icons','./media/icons/');
+define('background','./media/background/');
--- views/layouts/header.php
+++ views/layouts/header.php
@@ -26,3 +26,55 @@ $menus = $query->fetchAll(PDO::FETCH_CLASS, Menu::class);
 <?php foreach ($menus as $itemMenu) : ?>
 	<p><?= $itemMenu->getMenuName() ?> </p>
 <?php endforeach ?>
+
+<header class="header_container">
+	<div class="header_image">
+		<img src=<?= logo . 'logo_wcg.png' ?> srcset="./images/icons/logo_smal_mobile.png 450w," alt="logo wcg" />
+	</div>
+	<nav class="header_nav_container">
+		<ul class="link-navbar bg-color-shadox" id="main-menu">
+			<?php
+			foreach ($menus as $menu) {
+			?>
+				<li class="menu  <?php echo $menu['RUB_BACKGROUND'] . " " . $menu['RUB_FONT_COLOR'] ?> link  <?php echo ($menu['a_sous_rubrique'] == 1 ? "link-menu-grid overMenu" : ""); ?>">
+					<a href="#">
+						<img class="logo-menu" src="./media/icons/<?php echo $menu['med_ressource']; ?>" alt="logo" />
+						<?php echo $menu['RUB_LIBELLE']; ?>
+					</a>
+					<?php if ($menu['a_sous_rubrique']) {
+						$sql2 = "SELECT sous_rubrique.SRU_ID as id, SRU_TITRE  as title 
+								  FROM sous_rubrique 
+								  INNER JOIN status   ON status.STA_ID = sous_rubrique.STA_ID 
+								  WHERE sous_rubrique.RUB_ID = " . $menu['RUB_ID'] . " AND sous_rubrique.STA_ID =1
+								  ORDER BY SRU_ORDRE";
+						$db->sql($sql2);
+						$resultSousMenu = $db->getResult();
+					?>
+						<i class="fa-solid fa-plus icon-cog" id="btn-plus-sous-menu"></i>
+						<div class=" sous-menu <?php echo $menu['RUB_BACKGROUND'] ?> ">
+							<div class="container-sous-menu-grid">
+								<ul class="link-navbar">
+									<?php
+									foreach ($resultSousMenu as $itemSousMenu) {
+									?>
+										<li class="link <?php echo $menu['RUB_FONT_COLOR'] ?>">
+											<a class="" href="#">
+												<?php echo $itemSousMenu['title']; ?>
+											</a>
+
+										</li>
+									<?php }
+									?>
+								</ul>
+							</div>
+						</div>
+					<?php } ?>
+				<?php
+			}
+				?>
+				</li>
+		</ul>
+	</nav>
+	<div class="fas fa-bars" id="menu-btn">
+	</div>
+</header>
