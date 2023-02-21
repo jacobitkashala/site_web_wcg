8074d45f9b6b6137d866c2248dfc6e7a4c65ab96
JacobitKashala <Jacobitkashala@gmail.com>
2023-02-21 15:31:05 +0100

    refa

--- database_v4.sql
+++ database_v4.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
 --
--- Host: localhost    Database: wcg_site_web_databases
+-- Host: localhost    Database: wcg_site_web_database
 -- ------------------------------------------------------
 -- Server version	8.0.32-0ubuntu0.22.04.2
 
@@ -14,6 +14,62 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Candidat`
+--
+DROP DATABASE IF EXISTS wcg_site_web_database;
+CREATE DATABASE wcg_site_web_database;
+use wcg_site_web_database;
+DROP TABLE IF EXISTS `Candidat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Candidat` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Email` varchar(255) NOT NULL,
+  `nom_complet` varchar(255) NOT NULL,
+  `telephone` varchar(255) NOT NULL,
+  `Date_naissance` date NOT NULL,
+  -- `Sexe` varchar(2) NOT NULL,
+  `SRU_ID` int NOT NULL,
+  `Note` text,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Candidat`
+--
+
+LOCK TABLES `Candidat` WRITE;
+/*!40000 ALTER TABLE `Candidat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Candidat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Partenaire`
+--
+
+DROP TABLE IF EXISTS `Partenaire`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Partenaire` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `media_id` int NOT NULL,
+  `description` text,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Partenaire`
+--
+
+LOCK TABLES `Partenaire` WRITE;
+/*!40000 ALTER TABLE `Partenaire` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Partenaire` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Temporary view structure for view `V_Contenu_Expertise`
@@ -721,4 +777,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-17 17:15:35
+-- Dump completed on 2023-02-21 15:27:43
--- src/Connection.php
+++ src/Connection.php
@@ -6,6 +6,22 @@ use \PDO;
 
 class Connection
 {
+	   /* 
+     * Create variables for credentials to MySQL database
+     * The variables have been declared as private. This
+     * means that they will only be available with the 
+     * Database class
+     */
+
+    // static private $db_host = "127.0.0.1";
+    // static private $db_user = "jaco";
+    // static private $db_pass = "1234";
+    // static private $db_name = "wcg_site_web_databases";
+	// 
+	// private $db_host = "127.0.0.1";
+    // private $db_user = "jacobit.k";
+    // private $db_pass = "wcg2023!";
+    // private $db_name = "wcg_site_web_databases";
 
 	public static $pdo;
 
@@ -23,6 +39,7 @@ class Connection
 	public static function getPDO()
 	{
 		$pdo = new PDO("mysql:host=127.0.0.1;dbname=wcg_site_web_databases", 'jaco', '1234');
+		// $pdo = new PDO("mysql:host=127.0.0.1;dbname=wcg_site_web_databases", 'jacobit.k', 'wcg2023!');
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 		$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
