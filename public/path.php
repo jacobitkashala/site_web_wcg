f1b949a99397cbb2b71e15f9f89401e6e2fda440
JacobitKashala <Jacobitkashala@gmail.com>
2023-02-23 13:48:14 +0100

    contionner tous les ressources

--- public/path.php
+++ public/path.php
@@ -5,14 +5,20 @@ define('evideo', './media/video/');
 define('ephoto', './media/photo/');
 if (ROOT_HOST == 'http://localhost:8080/') {
 	define('pathCss', ROOT_HOST . 'css/');
+	define('pathJs',  ROOT_HOST . 'js/');
+	define('logo',  ROOT_HOST . 'media/logo/');
+	define('ecrousel',  ROOT_HOST . 'media/carousel/');
+	define('icons',  ROOT_HOST . 'media/icons/');
+	define('background',  ROOT_HOST . 'media/background/');
 } else {
 	define('pathCss', ROOT_HOST . 'public/css/');
+	define('pathJs',  ROOT_HOST . 'public/js/');
+	define('logo',  ROOT_HOST . 'public/media/logo/');
+	define('ecrousel',  ROOT_HOST . 'public/media/carousel/');
+	define('icons',  ROOT_HOST . 'public/media/icons/');
+	define('background',  ROOT_HOST . 'public/media/background/');
 }
-define('pathJs', './js/');
-define('logo', './media/logo/');
-define('ecrousel', './media/carousel/');
-define('icons', './media/icons/');
-define('background', './media/background/');
+
 define('siteId', 1);
 define('limite1', 1);
 define('limite2', 2);
