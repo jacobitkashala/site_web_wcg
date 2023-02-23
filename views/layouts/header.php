d09059c56b8b6d848fee2c632647786dc09dcd41
JacobitKashala <Jacobitkashala@gmail.com>
2023-02-23 17:04:29 +0100

    refactor:header

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -5,23 +5,12 @@ use App\MODEL\SousMenu;
 use App\Connection;
 
 
-$pdo = Connection::getPDO(db_host,db_user,db_pass,db_name);
+$pdo = Connection::getPDO(db_host, db_user, db_pass, db_name);
 $query = $pdo->query('SELECT  template.TPL_LIBELLE,r.a_sous_rubrique,r.url_page,r.RUB_ID, r.RUB_ICONE_ID, r.RUB_LIBELLE,r.RUB_BACKGROUND,r.RUB_FONT_NAME,r.RUB_FONT_SIZE,r.RUB_FONT_COLOR, m.med_ressource FROM rubrique r, media m, site s ,template WHERE m.med_id = r.rub_icone_id and r.SIT_ID = s.SIT_ID and s.SIT_ID = 1 and template.TPL_ID = r.TPL_ID ORDER BY r.RUB_ORDRE');
 $menus = $query->fetchAll(PDO::FETCH_CLASS, Menu::class);
 
 $newPathLogo = logo;
 $newPathIcons = icons;
-// $newPathEphoto = ephoto;
-
-// if ($_SERVER['REQUEST_URI'] != '/') {
-// 	$newPathLogo = "." . $newPathLogo;
-// 	$newPathIcons = "." . $newPathIcons;
-// 	// $newPathEphoto  = ".".$newPathEphoto;
-// }
-
-// echo db_host;
-// echo print_r(dirname(__DIR__));
-// echo '<pre>';
 
 ?>
 <header class="header_container">
@@ -35,21 +24,25 @@ $newPathIcons = icons;
 					<a href="<?= $router->url($itemMenu->getMenuTemplate(), array('id' => $itemMenu->getMenuId(), 'slug' => $itemMenu->getMenuSlug())) ?>">
 						<img class="logo-menu" src="<?= $newPathIcons . $itemMenu->getMenuNameIcone(); ?>" alt="logo" />
 						<?= $itemMenu->getMenuName()  ?>
+						<?php if ($itemMenu->getIsSubMenu() == 1) : ?>
+							<i class="fa-solid fa-plus icon-cog" id="btn-plus-sous-menu"></i>
+						<?php endif ?>
+						<!-- <i class="fa fa-angle-right pull-right"></i> -->
 					</a>
 					<?php if ($itemMenu->getIsSubMenu() == 1) {
 						$sql2 = "SELECT sous_rubrique.SRU_ID as id, SRU_TITRE  as title , template.TPL_LIBELLE as url_sub_menu, template.TPL_RESSOURCES as ressource_sub_menu FROM sous_rubrique INNER JOIN status   ON status.STA_ID = sous_rubrique.STA_ID INNER JOIN template   ON template.TPL_ID = sous_rubrique.TPL_ID WHERE sous_rubrique.RUB_ID = " . $itemMenu->getMenuId() . " AND sous_rubrique.STA_ID = 1 ORDER BY SRU_ORDRE";
 						$query = $pdo->query($sql2);
 						$subMenus = $query->fetchAll(PDO::FETCH_CLASS, SousMenu::class);
 					?>
-						<i class="fa-solid fa-plus icon-cog" id="btn-plus-sous-menu"></i>
+
 						<div class="sous-menu <?php echo $itemMenu->getMenuBackgroud() ?> ">
 							<div class="container-sous-menu-grid">
 								<ul class="element-sous-menu link-navbar">
 									<?php foreach ($subMenus as $itemSousMenu) : ?>
 										<li class="  <?= $itemMenu->getMenuFontColor() ?>">
-											<!-- <li class="link <?= $itemMenu->getMenuFontColor() ?>"> -->
 											<a class="" href="<?= $router->url($itemMenu->getMenuTemplate(), array('id' => $itemSousMenu->getSubMenuId(), 'slug' => $itemMenu->getMenuId())) ?>">
 												<?php echo $itemSousMenu->getSubMenuTitle(); ?>
+
 											</a>
 										</li>
 									<?php endforeach ?>
--- views/little_content.php
+++ views/little_content.php
@@ -4,7 +4,7 @@ use App\MODEL\LittleContent;
 use App\Helpers\Text;
 use App\Connection;
 
-$pdo = Connection::getPDO(db_host,db_user,db_pass,db_name);
+$pdo = Connection::getPDO(db_host, db_user, db_pass, db_name);
 
 $titlePage = 'Little content';
 $descriptionPage = 'World Corp Group est une société de consulting et de développement de projets innovants, spécialiste en solutions SMAC (Social, Mobile, Analytics, Cloud)';
@@ -16,19 +16,14 @@ $queryLittleContent = "";
 
 if ($idParent === 0) {
 	$queryLittleContent = $pdo->query("SELECT rubrique.SIT_ID as id, media.MED_RESSOURCE as bgImage, rubrique.RUB_LIBELLE as libelle,rubrique.RUB_TITRE as titre ,rubrique.RUB_CONTENU  as contenu FROM rubrique INNER JOIN media ON media.MED_ID = rubrique.RUB_IMG_ID WHERE rubrique.RUB_ID = " . $idMenu . " AND rubrique.SIT_ID = 1 AND rubrique.STA_ID = 1 LIMIT 1");
-
-}else if($idParent > 0){
-	$queryLittleContent = $pdo->query("SELECT media.MED_RESSOURCE as bgImage, sous_rubrique.SRU_LIBELLE as libelle,sous_rubrique.SRU_TITRE as titre ,sous_rubrique.SRU_CONTENU  as contenu FROM sous_rubrique INNER JOIN media ON media.MED_ID = sous_rubrique.SRU_IMG_ID WHERE sous_rubrique.RUB_ID = " . $idParent . " AND sous_rubrique.SRU_ID = ". $idMenu." AND sous_rubrique.STA_ID = 1 LIMIT 1");	
-
+} else if ($idParent > 0) {
+	$queryLittleContent = $pdo->query("SELECT media.MED_RESSOURCE as bgImage, sous_rubrique.SRU_LIBELLE as libelle,sous_rubrique.SRU_TITRE as titre ,sous_rubrique.SRU_CONTENU  as contenu FROM sous_rubrique INNER JOIN media ON media.MED_ID = sous_rubrique.SRU_IMG_ID WHERE sous_rubrique.RUB_ID = " . $idParent . " AND sous_rubrique.SRU_ID = " . $idMenu . " AND sous_rubrique.STA_ID = 1 LIMIT 1");
 }
 
 $resultquery = $queryLittleContent->fetchAll(PDO::FETCH_CLASS, LittleContent::class);
 
 $newPathEphoto = ephoto;
 
-if ($_SERVER['REQUEST_URI'] != '/') {
-	$newPathEphoto = "." . $newPathEphoto;
-}
 
 echo "
 <style>
@@ -44,9 +39,9 @@ background-repeat: no-repeat;
 //  echo '<pre>';
 //  exit();
 ?>
-<section class="container_content_expertise">
-	<h4> <?=  $resultquery[0]->getLittTitle() ?></h4>
-	<p>
+<section class=" row container_content_expertise">
+	<h4 class=" col-log-10 col-md-10 col-sm-10"> <?= $resultquery[0]->getLittTitle() ?></h4>
+	<p class=" col-log-10 col-md-10 col-sm-10">
 		<?= $resultquery[0]->getLittContenu() ?>
 	</p>
 </section>
