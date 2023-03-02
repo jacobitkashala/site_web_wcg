6f93fb9c4f9c5799f2409098ff004b285b86bde2
JacobitKashala <Jacobitkashala@gmail.com>
2023-03-02 09:38:49 +0100

    add:style

--- views/admin/ressources.php
+++ views/admin/ressources.php
@@ -7,20 +7,40 @@ use App\Connection;
 $pdo = Connection::getPDO(db_host, db_user, db_pass, db_name);
 
 //On passe a la variable page 1 s'il n'existe pas et 1 s'il contient ne porte quoi
-$currentPage = (int)($_GET['page'] ?? 1) ?: 1;
+$page = $_GET['page'] ?? 1;
+
+// gestion de nombre a virgile
+if (!filter_var($page, FILTER_VALIDATE_INT)) {
+	header("Location: " . $router->url('ressources'));
+	http_response_code(301);
+	exit();
+	// throw new Exception("Numéro de page invalide");
+};
+
+if ($page === '1') {
+	header("Location: " . $router->url('ressources'));
+	http_response_code(301);
+	exit();
+}
+
+// $currentPage = (int)($_GET['page'] ?? 1) ?: 1;
+$currentPage = (int)($page);
 
 if ($currentPage <= 0) {
-	throw new Exception("Numéro de page invalide");
+	header("Location: " . $router->url('ressources'));
+	http_response_code(301);
+	exit();
+	// throw new Exception("Numéro de page invalide");
 }
-$slqRessourcesCount = "SELECT COUNT(*) FROM  media m inner join type_media tm ON tm.TYM_ID=m.TYM_ID LIMIT 10;";
-$countRessources = (int)$pdo->query($slqRessources)->fetch(PDO::FETCH_NUM)[0];
+
+$countRessources = (int)$pdo->query("SELECT COUNT(*) FROM  media m inner join type_media tm ON tm.TYM_ID=m.TYM_ID ")->fetch(PDO::FETCH_NUM)[0];
 $nbreElment = 10;
 $page = ceil($countRessources / $nbreElment);
 
-if($currentPage> $page){
+if ($currentPage > $page) {
 	throw new Exception("Cette page n'existe pas");
 }
-$offset = $nbreElment * ($currentPage -1);
+$offset = $nbreElment * ($currentPage - 1);
 $slqRessources = "SELECT m.MED_LIBELLE as libelle, m.MED_RESSOURCE ressources,m.MED_INFOBULLE  as infobulle ,m.MED_META as metadesc ,tm.TYM_LIBELLE as nomtype FROM  media m inner join type_media tm ON tm.TYM_ID=m.TYM_ID LIMIT $nbreElment OFFSET  $offset";
 
 $queryRessources = $pdo->query($slqRessources);
@@ -75,7 +95,7 @@ $ressources = $queryRessources->fetchAll(PDO::FETCH_CLASS, Ressources::class);
 					foreach ($ressources as $ressource) : ?>
 						<tr>
 							<td>
-								<?= $i++ ?></td>
+								<?= $i++ ?>
 							</td>
 							<td>
 								<?= $ressource->getNomtype() ?? "Manque info" ?></td>
@@ -100,16 +120,20 @@ $ressources = $queryRessources->fetchAll(PDO::FETCH_CLASS, Ressources::class);
 								<a class="btn btn-danger m-1" href="#">Supprimer</a>
 							</td>
 						<?php endforeach ?>
+				<tfoot>
+					<tr>
+						<?php if ($currentPage > 1) : ?>
+							<th class=""><a class="btn btn-primary mx-4" href="<?= $router->url('ressources') ?>?page=<?= ($currentPage - 1) ?>"> Page précédente </a></th>
+						<?php endif ?>
+						<?php if ($currentPage < $page) : ?>
+							<th>
+								<a class="btn btn-primary" href="<?= $router->url('ressources') ?>?page=<?= ($currentPage + 1) ?>"> Page suivant </a>
+							</th>
+						<?php endif ?>
+					</tr>
+				</tfoot>
 				</tbody>
 			</table>
-		</div>
-		<div class="d-flex justify-content-betwen my-4">
-			<?php if ($currentPage > 1) : ?>
-				<a class="btn btn-primary" href="<?= $router->url('ressources') ?>?page<?= $currentPage - 1 ?>"> Page précédente </a>
-			<?php endif ?>
-			<?php if ($currentPage < $page) : ?>
-				<a class="btn btn-primary" href="<?= $router->url('ressources') ?>?page<?= $currentPage + 1 ?>"> Page suivant </a>
-			<?php endif ?>
 		</div>
 	</div>
 </main>
