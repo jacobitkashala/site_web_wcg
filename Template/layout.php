cdb1cde893ccf2e11db4e69f401b92dc4af77a88
JacobitKashala <Jacobitkashala@gmail.com>
2023-03-16 15:19:13 +0100

    home mvp

---
+++ Template/layout.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>World Corp Group</title>
+
+	<meta charset='utf-8'>
+	<meta name="description" content="World Corp Group est une société de consulting et de développement de projets innovants, spécialiste en solutions SMAC (Social, Mobile, Analytics, Cloud)" />
+	<meta name="autheur" content="jacobit,kashala,serge kashala,jacobit kashala " />
+	<meta name="keywords" content="World Corp Group, WorldGroupCorp,WorldCorpGroup" />
+	<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<meta property="og:title" content="World Corp Group" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="http://www.example.com/" />
+	<meta property="og:image" content="http://example.com/image.jpg" />
+	<meta property="og:description" content="World Corp Group est une société de consulting et de développement de projets innovants, spécialiste en solutions SMAC (Social, Mobile, Analytics, Cloud)" />
+
+	<!-- Twitter Card data -->
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="@publisher_handle">
+	<meta name="twitter:title" content="World Corp Group">
+	<meta name="twitter:description" content="World Corp Group est une société de consulting et de développement de projets innovants, spécialiste en solutions SMAC (Social, Mobile, Analytics, Cloud)">
+	<meta name="twitter:creator" content="jacobitkashala">
+	<!-- Twitter Summary card images must be at least 200x200px -->
+	<meta name="twitter:image" content="http://www.example.com/image.jpg">
+	<!-- font -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,900&display=swap" rel="stylesheet">
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.compat.css" />
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+	<link rel='stylesheet' type='text/css' media='screen' href="./style/slick.css">
+	<link rel='stylesheet' type='text/css' media='screen' href="./style/slick-theme.css">
+
+	<link rel='stylesheet' type='text/css' media='screen' href='./style/header.css'>
+	<link rel='stylesheet' type='text/css' media='screen' href='./style/home.css'>
+	<link rel='stylesheet' type='text/css' media='screen' href='./style/footer.css'>
+	<link rel="shortcut icon" href="./media/icons/favico.ico">
+</head>
+
+
+<body class="body">
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+<script src="./js/main.js"></script>
+<script src="./js/slick.min.js "></script>
+<script src="./js/slick.js"></script>
+
+
+</html>
---
+++ elements/header.php
@@ -0,0 +1,86 @@
+<!-- Le header -->
+<header class="header_container ">
+	<div class="header_image">
+		<a class="" href="#">
+			<img src="./media/logo/logo_wcg.png" alt="logo wcg" />
+		</a>
+	</div>
+	<nav class="header_nav_container ">
+		<ul class="link-navbar bg-color-shadox" id="main-menu">
+			<li class="menu  link-menu-grid overMenu   " style="background-color:#BF24C0">
+				<a class=" menu-show-no-mo  " href="./pages/expertise.html" style="color:#FFFFFF ">
+					<img class="logo-menu " src="./media/icons/expertise_menu.png" alt="logo" />
+					Expertises
+				</a>
+				<i class="  fa-solid fa-plus icon-cog" id="btn-plus-sous-menu"></i>
+
+				<div class="grid-item sous-menu  " style="background-color:#701475">
+					<div class="underlin">
+					</div>
+					<div class="container-sous-menu-grid">
+						<ul class="element-sous-menu link-navbar">
+							<li class="sub-menu">
+								<a href="./pages/expertise.html" style="color:#FFFF">
+									CONSULTING
+								</a>
+							</li>
+							<li class="sub-menu">
+								<a href="./pages/expertise.html" style="color:#FFFF">
+									CLOUD & DATA
+								</a>
+							</li>
+							<li class="sub-menu">
+								<a href="./pages/expertise.html" style="color:#FFFF">
+									DIGITAL & INNOVATION
+								</a>
+							</li>
+							<li class="sub-menu">
+								<a href="./pages/expertise.html" style="color:#FFFF">
+									DESIGN
+								</a>
+							</li>
+							<li class="sub-menu">
+								<a href="./pages/expertise.html" style="color:#FFFF">
+									TELECOM
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</li>
+			<li class="menu link   " style="background-color:#3F079C">
+				<a class="" href="./pages/expertise.html" style="color:#FFFFFF ">
+					<img class="logo-menu" src="./media/icons/secteur_menu.png" alt="logo" />
+					Secteurs
+				</a>
+			</li>
+			<li class="menu link " style="background-color:#2592F9">
+				<a class="" href="./pages/expertise.html" style="color:#FFFFFF ">
+					<img class="logo-menu " src="./media/icons/innovation_menu.png" alt="logo" />
+					Innovations
+				</a>
+			</li>
+			<li class="menu link " style="background-color:#14D1B3">
+				<a class="" href="./pages/recrutement.html" style="color:#FFFFFF ">
+					<img class="logo-menu" src="./media/icons/recrutement_menu.png" alt="logo" />
+					Recrutements
+				</a>
+			</li>
+			<li class="menu link  " style="background-color:#FFB400">
+				<a class="" href="./pages/expertise.html" style="color:#FFFFFF ">
+					<img class="logo-menu " src="./media/icons/ressource_menu.png" alt="logo" />
+					Ressources
+				</a>
+			</li>
+			<li class="menu link " style="background-color:#FF0000">
+				<a class="" href="./pages/actualite.html" style="color:#FFFFFF ">
+					<img class="logo-menu" src="./media/icons/actualite_menu.png" alt="logo" />
+					Actualités
+				</a>
+			</li>
+
+		</ul>
+	</nav>
+	<div class="fas fa-bars" id="menu-btn">
+	</div>
+</header>
